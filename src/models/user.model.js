f75d6e163d542233b15d38cb82a5f6ab520e0c3e
Andrea Massun Sovic <amassunsovic@gmail.com>
2026-06-07 03:57:56 -0300

    'Tabla' Usuario

---
+++ src/models/user.model.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+    name: {
+    type: String,
+    required: true,
+    trim: true
+     },
+     
+     email: {
+        type: String,
+        required: true,
+        unique: true,
+        unique: true,
+        lowercase: true,
+    },
+
+    password: {
+        type: String,
+        required: true,
+    },
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
